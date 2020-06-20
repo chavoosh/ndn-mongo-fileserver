c0940853bbb82108ff9f89d8601c7911c7c04924
Chavoosh Ghasemi <chghasemi@cs.arizona.edu>
2020-06-19 20:42:24 -0700

    scripts: remastered parser.py

--- scripts/results/parser.py
+++ scripts/results/parser.py
@@ -8,14 +8,17 @@
 #
 # DESCRIPTION:
 # ------------
-# This script parses the log file collected by stats-collector tool
-# and print each line of the log file in a specific format. Look at
-# the structure of each printed line, below:
+# This script parses the log file collected by stats-collector tool.
+# It returns an array containing video sessions sorted based on date and time.
+# Each video session is a separate array containing parsed lines (i.e., records)
+# of the corresponding session.
 #
-# OUTPUT STRUCTURE
+# To learn about the format of each record look at the following description.
+#
+# OUTPUT FORMAT
 # --------------
 # $1 : Date & Time
-# $2 : File name
+# $2 : File/Content name
 # $3 : Status (=[DONE] OR [FAIL])
 # $4 : Hub
 # $5 : Client IP address
@@ -34,11 +37,9 @@
 #.....................................................................
 
 import os
-import sys
-import getopt
-import subprocess
+import datetime
 
-class bcolors:
+class BColors:
     OKBLUE = '\033[94m'
     OKGREEN = '\033[92m'
     WARNING = '\033[93m'
@@ -47,67 +48,151 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-prefix_size = 4 # /ndn/web/stats/video
-ignore_buf_dur = True
-log_file=""
+class Input_Options:
+    plen = 3              # /ndn/video/stats
+    ignore_buf = True     # ignore all buffer durations
+    print_records = False # do not print the records
 
-if len(sys.argv) == 1:
-    print "program usage:\n\tpython parser.py <log-file>\n",\
-          "\t-s: size of fixed prefix\n",\
-          "\t-b: print buffering duration (if any exists)\n"
-    sys.exit(2)
+# CONST
+NUMBER_OF_FIELDS = 16
+FIELDS = {
+    'Date': 0,
+    'Name': 1,
+    'Stat': 2,
+    'Hub' : 3,
+    'Cip' : 4,
+    'Ebw' : 5,
+    'Retx': 6,
+    'Tout': 7,
+    'Nack': 8,
+    'Segm': 9,
+    'Retr': 10,
+    'Rtt' : 11,
+    'Jitt': 12,
+    'Sid' : 13,
+    'Strt': 14,
+    'Reb' : 15}
 
-try:
-    opts, args = getopt.getopt(sys.argv[2:], "s:b")
-except getopt.GetoptError:
-    print "program usage:\n\tpython parser.py <log-file>\n",\
-          "\t-s: size of fixed prefix\n",\
-          "\t-b: print buffering duration (if any exists)\n"
-    sys.exit(2)
-for opt, arg in opts:
-    if opt == '-s':
-        prefix_size = int(arg)
-    elif opt == '-b':
-        ignore_buf_dur = False
+MONTHS = {
+    'JAN': 1,
+    'FEB': 2,
+    'MAR': 3,
+    'APR': 4,
+    'MAY': 5,
+    'JUN': 6,
+    'JUL': 7,
+    'AUG': 8,
+    'SEP': 9,
+    'OCT': 10,
+    'NOV': 11,
+    'DEC': 12}
 
-log_file = sys.argv[1]
-invalid_records = []
-try:
-    f = file(log_file, "r")
+
+
+'''
+  Group the lines belonging to each video session in separate arrays in $session_map.
+'''
+def situate_record(record, session_map):
+    sid = record[13]
+    if sid not in session_map:
+        session_map[sid] = []
+        session_map[sid].append(record)
+    else:
+        session_map[sid].append(record)
+
+'''
+  $arg can be either a record or a session (i.e., a group of records)
+'''
+def get_record_datetime(arg):
+    return arg[0] if isinstance(arg[0], datetime.datetime) else arg[0][0]
+
+'''
+  Sort records of each session timewise and put them in $rc. Then, sort $rc based on the
+  starting time of each session.
+'''
+def sort_sessions(session_map, rc):
+    for sid in session_map:
+        session_map[sid] = sorted(session_map[sid], key=get_record_datetime)
+        rc.append(session_map[sid])
+    rc = sorted(rc, key=get_record_datetime)
+
+def parser(log, inopts):
+    invalid_lines = [] # contain lines that cannot be parsed
+    session_map = {}   # group each session's lines in an array ([session_id][lines])
+    rc = []            # sort out the lines according to session id and time ([x][session's sorted lines])
+
+    f = file(log, "r")
     line = f.readline()
     while line:
-        record = ""
+        record = [] # parsed line
         line = line.rstrip()
         line = line.replace('  ', ' ') # equivalent to sed
+        if line == '':
+            line = f.readline()
+            continue
         phrases = line.split(' ')
-        # time stamp always have a fixed position
-        record += phrases[2] + '-' + phrases[1] + '-' + phrases[4] + '_' + phrases[3] + ' '
 
-        # afte time stamp the stats starts
-        stats = phrases[5].split('/')
-        stats = filter(None, stats)
-        if prefix_size > len(stats):
-            print "ERROR: prefix size is greater than the file name!\n"
+        try:
+          dt = datetime.datetime(
+            int(phrases[4]),
+            MONTHS[phrases[1].upper()],
+            int(phrases[2]),
+            int(phrases[3].split(':')[0]),
+            int(phrases[3].split(':')[1]),
+            int(phrases[3].split(':')[2]))
+        except: # invalid record
+            invalid_lines.append(BColors.WARNING + '[Invalid Date/Time] ' + BColors.ENDC + BColors.ENDC + line)
+            line = f.readline()
+            continue
+        record.append(dt)
+
+        stats = filter(None, phrases[5].split('/'))
+        if inopts.plen > len(stats):
+            print "ERROR: prefix length is greater than solicited content's name!\n"
             sys.exit(2)
 
-        for i in range(prefix_size, len(stats)):
-            # check whether to print out the rebuffering durations (this can be long)
-            if ignore_buf_dur and stats[i].find('bufferingDuration') != -1:
+        content_name = '' # name of solicited content
+        nit = inopts.plen
+        while stats[nit].find('%') == -1 and nit < len(stats):
+            content_name += '/' + stats[nit]
+            nit += 1 # name component iterator
+        record.append(content_name)
+
+        valid = True
+        num_of_fields = 2 # we already have date/time and content's name
+        for fit in range(nit, len(stats)):
+            if stats[fit].find('bufferingDuration') != -1 and inopts.ignore_buf:
                 break
-            if stats[i].find('%') == -1:
-                record += '/' + stats[i]
-            elif stats[i].find('%3D') != -1:
-                if stats[i].split('%3D')[0] == 'session' and stats[i].split('%3D')[1].isdigit() == False:
-                    invalid_records.append(bcolors.FAIL + 'SESSION ID is not valid. ' + bcolors.ENDC  +  'RECORD: ' + line)
-                elif stats[i].split('%3D')[1] == '':
-                    record += ' NULL'; # if a key does not have any value
+            elif stats[fit].find('%3D') != -1:
+                if stats[fit].split('%3D')[0] == 'session' and stats[fit].split('%3D')[1].isdigit() == False:
+                    invalid_lines.append(BColors.WARNING + '[Invalid Session ID] ' + BColors.ENDC + line)
+                    valid = False
+                    break
+                elif stats[fit].split('%3D')[1] == '':
+                    record.append('NULL'); # if a key does not have any value
                 else:
-                    record += ' ' + stats[i].split('%3D')[1]
-        print record
+                    record.append(stats[fit].split('%3D')[1])
+            else:
+                invalid_lines.append(BColors.WARNING + '[Misplaced/Invalid Field (' +\
+                        str(stats[fit]) + ')] ' + BColors.ENDC + line)
+                valid = False
+                break
+            num_of_fields += 1
+
+        if num_of_fields < NUMBER_OF_FIELDS and valid:
+            invalid_lines.append(BColors.WARNING + '[Too Few Fields (' + str(num_of_fields) +\
+                    '<' + str(NUMBER_OF_FIELDS) + ')] ' + BColors.ENDC + line)
+            valid = False
+
+        if valid:
+            situate_record(record, session_map)
+            if inopts.print_records:
+                print record
         line = f.readline()
-finally:
-    if len(invalid_records) > 0:
-        print(bcolors.WARNING + 'WARNING: List of invalid records during parsing:' + bcolors.ENDC)
-        for r in invalid_records:
-            print r
     f.close()
+    sort_sessions(session_map, rc)
+    if len(invalid_lines) > 0:
+        print(BColors.BOLD + '--------------\nINVALID LINES:\n--------------' + BColors.ENDC)
+        for l in invalid_lines:
+            print l
+    return rc
