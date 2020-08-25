8d7b24a7b9d1f3cfd766b8fc7afb6d0ea86c11ba
Chavoosh Ghasemi <chghasemi@cs.arizona.edu>
2020-08-25 14:50:08 -0700

    scripts: print exiting code in packager.sh

--- scripts/video/packager.sh
+++ scripts/video/packager.sh
@@ -7,31 +7,35 @@
 # this script e.g., in COPYING.md file. If not, see <http://www.gnu.org/licenses/>.
 #...............................................................
 
-set -e
+error=""
 
 if [ $# -lt 4 ]; then
   echo -e "\ndash-packager usage:
-           \t1st arg: input directory
-           \t2nd arg: prefix of input files
+           \t1st arg: input directory (including encoded files)
+           \t2nd arg: name of the original video file
            \t3rd arg: target directory (to save the output files)
            \t4th arg: protocol (dash || hls)
-           \tNOTE:    adjust resolution in the script\n"
+           \tNOTE:    adjust output resolutions in the script\n"
   exit -1
 fi
 
 if [ ! -d $1 ]; then
-  echo -e "\nError:\n\tdirectory ${1} does not exist\n"
+  error="${1}: Directory does not exist..."
+  echo -e "\e[31mECode:1 (FAILURE)\n$error\e[39m"
   exit -1
 fi
 
-if [ $4 == "dash" ] && [ $4 == "hls" ]; then
-  echo -e "\nError:\n\t${4} is not a valid protocol (choose dash OR hls)\n"
+if [ $4 != "dash" ] && [ $4 != "hls" ]; then
+  error="${4}: Not a valid protocol (choose dash OR hls)"
+  echo -e "\e[31mECode:1 (FAILURE)\n$error\e[39m"
   exit -1
 fi
 
 if [ ! -d $3 ]; then
-  echo -e "\nWarning:\n\tdirectory ${3} does not exist, so it will be created\n"
+  echo -e "\e[33mWarning:\n${3}: Directory does not exist. It will be created...\e[39m"
 fi
+
+logFile=".preport.log"
 
 # comment/add lines here to control which renditions would be created
 renditions=(
@@ -81,5 +85,16 @@ else
   cmd+=( --hls_master_playlist_output ${3}/playlist.m3u8)
 fi
 
-echo -e "Executing command:\npackager ${cmd[@]}"
-packager "${cmd[@]}" --segment_duration 2.5 
+echo -e "Exec command:\n\e[2mpackager ${cmd[@]}\e[0m"
+packager "${cmd[@]}" --segment_duration 2.5 &> $logFile
+
+ll=$(tail -n 1 $logFile)
+echo $ll
+if [[ $ll == *"Error"* ]]; then
+  error=$ll
+  echo -e "\e[31mECode:1 (FAILURE)\n$error\e[39m"
+  rm -rf $3
+else
+  echo -e "\e[32mECode:0 (SUCCESS)\e[39m"
+fi
+rm $logFile
